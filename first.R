87990550314e9eb20b301fe60dc13490efc5c5be
pd1262 <pd1262@pjwskt.edu.pl>
2021-11-07 16:30:54 +0100

    zad 1 i 2

--- first.R
+++ first.R
@@ -1,1 +1,175 @@
 print("hello")
+
+
+class(x)
+str(x)
+
+is.vector(x)
+
+?vector
+
+x<-c(1,2,3,4,5,6,7,8,9,10)
+x
+x<-seq(1:10)
+?seq
+
+x<-seq(1,10, by=1)
+y<-seq(1,10, by=1)
+
+z<-c(x,y)
+
+z
+x
+is.numeric(x)
+xx<-as.character(x) # zamiana
+
+zz<-c(x,xx)
+
+zz
+
+as.numeric(zz)
+
+as.logical(z)
+as.logical(0)
+
+z <-c(0,z)
+
+nowy<-as.numeric(w)
+nowy
+
+nowy2<- c("raz", "dwa", "trzy")
+
+lista <-list(z, nowy, nowy2)
+lista
+lista[[3]]
+
+class(lista[[3]])
+
+lista[[3]][2]
+
+for (i in 1:length(z)) {
+    print(z[i])
+}
+
+
+for (i in 1:length(lista)) {
+  for (j in 1:length(lista[[i]])){
+  print(lista[[i]][i])
+  }
+}
+
+# brak biblioteki
+?pow2
+
+install.packages("pracma")
+
+# ładujemy jako paczka, wiec bez ""
+library(pracma)
+
+#wersja
+version
+
+pow2(x)
+
+x<-1
+while (i<=length(x)) {
+  print(pow2(x[i]))
+  i <- i +1
+}
+
+
+
+#data.frame
+df<-data.frame( 1, "2", 3)
+str(df)
+
+df<-data.frame(id= 1, "2", 3)
+str(df)
+
+
+
+
+df <- data.frame(1:10,1,2,3)
+df
+
+getwd()
+
+?read.csv
+
+
+setwd('/home/marcin/Dokumenty/PJAP/R')
+
+dfFromFileZle <-read.csv("./dane.csv")
+colnames(df)<-c("wzrost", "waga", "wiek")
+
+View(dfFromFileZle)
+
+str(df)
+
+dfFromFile<-read.csv("./dane.csv", TRUE,";")
+
+View(dfFromFile)
+
+
+hello<-function(x = "imie") {
+  print(paste0("hello ", x))
+}
+
+?paste0
+
+hello(x="lukasz")
+
+txt<-"lukasz2"
+hello(x=txt)
+
+hello()
+
+
+# sum(x)
+# x^2
+# mean(x)
+# lenagth(x)
+wariancja <-fukction(x){
+  #....
+}
+?var
+dfFromFile[1,1]
+dfFromFile[,1]
+dfFromFile[1,]
+dfFromFile[,3]
+dfFromFile$wiek
+
+
+
+maciez<-matrix(x,nrow=10, ncol = 10)
+x
+y<-2
+y<-c(2,4)
+
+
+
+
+# +, -, *, /, %%, %/% - jak działają?
+
+
+?readline
+
+odp <-readline("Are you satisfied R user?") # yes,no
+strsplit(odp, split=",")
+
+odp1 <-strsplit(odp, split=",")[[1]][1]
+odp2 <-strsplit(odp, split=",")[[1]][2]
+
+
+odp1
+odp2
+
+v<- strsplit(odp, split=",")[[1]][1]
+
+podpowiedzFrame<-data.frame(matrix(ncol= 3, nrow=0))
+colnames(podpowiedzFrame) <- v
+
+
+podpowiedzFrame
+rbind
+cbind
--- zadania1.R
+++ zadania1.R
@@ -1,22 +1,37 @@
 #1. Napisz funkcję sprawdzająca czy 1 liczba jest podzielna przez druga użyj - %%
+czy_podzielna <-function(a, b){
+  if (a%% b ==0){
+    print (paste0("liczba ",a," przez " , b," jest podzielna"))
+  } else {
+    print (paste0("liczba ",a," przez " , b," nie jest podzielna"))
+  }
+}
+
+czy_podzielna(3)
+
 
 #2. Pociąg z Lublina do Warszawy przejechał połowę drogi ze średnią prędkością 120 km/h.
+# Drugą połowę przejechał ze średnią prędkością 90 km/h.
+# Jaka była średnia prędkość pociągu.
 
-Drugą połowę przejechał ze średnią prędkością 90 km/h.
+# Vsr = 2/(1/v1 + 1/v2)
 
-Jaka była średnia prędkość pociągu.
+Vsr<- 2/( 1/120 + 1/90)
+Vsr
 
 #3. Utwórz funkcję obliczającą współczynnik korelacji r Pearsona dla 2 wektorów o tej samej długości.
-Wczytaj dane plik dane.csv i oblicz współczynnik dla wagi i wzrostu. W komentarzu napisz co oznacza wynik.
+# Wczytaj dane plik dane.csv i oblicz współczynnik dla wagi i wzrostu. W komentarzu napisz co oznacza wynik.
 
+dfDane <- read.csv("./dane.csv", TRUE,";")
+View(dfDane)
 
 #4. Napisz funkcję zwracającą ramke danych z danych podanych przez użytkownika 
 #stworzDataFrame <- function(ile=1)
-W pierwszym wierszu użytkownik podaje nazwy kolumn. w kolejnych wierszach zawartość wierszy ramki danych ( tyle wierszy ile podaliśmy w argumencie ile. ile=1 oznacza, że gdy użytkownik nie poda żadnej wartości jako parametr, domyślna wartością będzie 1)
+# W pierwszym wierszu użytkownik podaje nazwy kolumn. w kolejnych wierszach zawartość wierszy ramki danych ( tyle wierszy ile podaliśmy w argumencie ile. ile=1 oznacza, że gdy użytkownik nie poda żadnej wartości jako parametr, domyślna wartością będzie 1)
 
 #5 Napisz funkcję , która pobiera sciezkeKatalogu, nazweKolumny, jakaFunkcje, DlaIluPlikow i liczy: 
 #mean, median,min,max w zależności od podanej nazwy funkcji w argumencie, z katologu który podaliśmy i z tylu plików ilu podaliśmy dla wybranej nazwy kolumny. 
-UWAGA: w podanych plikach R pobierając komórki nazwane liczbami R wstawi przed nazwy X. Funkcję przetestuj dla katalogu smogKrakow.zip.  Wykonując obliczenia pomiń brakujące wartości.
+#UWAGA: w podanych plikach R pobierając komórki nazwane liczbami R wstawi przed nazwy X. Funkcję przetestuj dla katalogu smogKrakow.zip.  Wykonując obliczenia pomiń brakujące wartości.
 
 liczZplikow <- function(sciezka,nazwaKolumny,jakaFunkcja="mean",DlaIluPlikow=1){ 
   
